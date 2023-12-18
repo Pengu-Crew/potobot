9b663f52f84134a291d340f40ce93b2242c7c3f8
JustEvil <thatjustevil@gmail.com>
2023-12-18 13:19:48 -0600

    let's make this shit work like the last week

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -1,20 +1,22 @@
-import { Command, CommandContext, Declare } from '@potoland/core';
+import { Command, CommandContext, Declare, PotoClient } from '@potoland/core';
 
 @Declare({
   name: 'ping',
   description: 'Gets the bot latency between discord and the.',
 })
 export default class PingCommand extends Command {
-  override async run(ctx: CommandContext<{}, []>) {
-    await ctx.interaction.deferReply();
+  override async run(ctx: CommandContext<true, {}, []>) {
+    const { interaction, client } = ctx;
+
+    await interaction.deferReply();
     return ctx.editResponse({
-      content: `Pong! \`${await this.getLatency()}ms\`.`,
+      content: `Pong! \`${await this.getLatency(client)}ms\`.`,
     });
   }
 
-  private async getLatency(): Promise<number> {
+  private async getLatency(client: PotoClient): Promise<number> {
     const start = Date.now();
-    await this.client.rest.get('/users/@me');
+    await client.rest.get('/users/@me');
     const end = Date.now();
 
     return end - start;
